b5d252dd2edac933f8069687a100faabfb734c1f
Michael Junior <69993392+Michael-Junior@users.noreply.github.com>
2021-02-07 02:08:28 -0300

    Criação da tela principal

---
+++ Principal_Calculadora/Principal_Calculadora/Form1.Designer.cs
@@ -0,0 +1,293 @@
+﻿namespace Principal_Calculadora
+{
+    partial class TelaPRINCIPAL
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TelaPRINCIPAL));
+            this.pictureBoxMENU = new System.Windows.Forms.PictureBox();
+            this.buttonICMS = new System.Windows.Forms.Button();
+            this.buttonICMSST = new System.Windows.Forms.Button();
+            this.buttonICMSDIFAL = new System.Windows.Forms.Button();
+            this.buttonIPI = new System.Windows.Forms.Button();
+            this.buttonPIS = new System.Windows.Forms.Button();
+            this.buttonCOFINS = new System.Windows.Forms.Button();
+            this.pictureBoxCALC = new System.Windows.Forms.PictureBox();
+            this.buttonSOBRE = new System.Windows.Forms.Button();
+            this.buttonINICIO = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxMENU)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxCALC)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxMENU
+            // 
+            this.pictureBoxMENU.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.pictureBoxMENU.Location = new System.Drawing.Point(-1, -1);
+            this.pictureBoxMENU.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBoxMENU.Name = "pictureBoxMENU";
+            this.pictureBoxMENU.Size = new System.Drawing.Size(173, 365);
+            this.pictureBoxMENU.TabIndex = 2;
+            this.pictureBoxMENU.TabStop = false;
+            // 
+            // buttonICMS
+            // 
+            this.buttonICMS.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.buttonICMS.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonICMS.FlatAppearance.BorderColor = System.Drawing.Color.DarkSlateGray;
+            this.buttonICMS.FlatAppearance.BorderSize = 0;
+            this.buttonICMS.FlatAppearance.MouseDownBackColor = System.Drawing.Color.CadetBlue;
+            this.buttonICMS.FlatAppearance.MouseOverBackColor = System.Drawing.Color.CadetBlue;
+            this.buttonICMS.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonICMS.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonICMS.ForeColor = System.Drawing.Color.White;
+            this.buttonICMS.Location = new System.Drawing.Point(-1, 28);
+            this.buttonICMS.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonICMS.Name = "buttonICMS";
+            this.buttonICMS.Size = new System.Drawing.Size(173, 34);
+            this.buttonICMS.TabIndex = 5;
+            this.buttonICMS.Text = "  ICMS";
+            this.buttonICMS.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.buttonICMS.UseVisualStyleBackColor = false;
+            this.buttonICMS.Click += new System.EventHandler(this.buttonICMS_Click);
+            // 
+            // buttonICMSST
+            // 
+            this.buttonICMSST.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.buttonICMSST.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonICMSST.FlatAppearance.BorderColor = System.Drawing.Color.DarkSlateGray;
+            this.buttonICMSST.FlatAppearance.BorderSize = 0;
+            this.buttonICMSST.FlatAppearance.MouseDownBackColor = System.Drawing.Color.CadetBlue;
+            this.buttonICMSST.FlatAppearance.MouseOverBackColor = System.Drawing.Color.CadetBlue;
+            this.buttonICMSST.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonICMSST.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonICMSST.ForeColor = System.Drawing.Color.White;
+            this.buttonICMSST.Location = new System.Drawing.Point(-1, 65);
+            this.buttonICMSST.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonICMSST.Name = "buttonICMSST";
+            this.buttonICMSST.Size = new System.Drawing.Size(173, 34);
+            this.buttonICMSST.TabIndex = 6;
+            this.buttonICMSST.Text = "  ICMS ST";
+            this.buttonICMSST.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.buttonICMSST.UseVisualStyleBackColor = false;
+            this.buttonICMSST.Click += new System.EventHandler(this.buttonICMSST_Click);
+            // 
+            // buttonICMSDIFAL
+            // 
+            this.buttonICMSDIFAL.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.buttonICMSDIFAL.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonICMSDIFAL.FlatAppearance.BorderColor = System.Drawing.Color.DarkSlateGray;
+            this.buttonICMSDIFAL.FlatAppearance.BorderSize = 0;
+            this.buttonICMSDIFAL.FlatAppearance.MouseDownBackColor = System.Drawing.Color.CadetBlue;
+            this.buttonICMSDIFAL.FlatAppearance.MouseOverBackColor = System.Drawing.Color.CadetBlue;
+            this.buttonICMSDIFAL.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonICMSDIFAL.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonICMSDIFAL.ForeColor = System.Drawing.Color.White;
+            this.buttonICMSDIFAL.Location = new System.Drawing.Point(-1, 103);
+            this.buttonICMSDIFAL.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonICMSDIFAL.Name = "buttonICMSDIFAL";
+            this.buttonICMSDIFAL.Size = new System.Drawing.Size(173, 34);
+            this.buttonICMSDIFAL.TabIndex = 7;
+            this.buttonICMSDIFAL.Text = "  ICMS DIFAL";
+            this.buttonICMSDIFAL.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.buttonICMSDIFAL.UseVisualStyleBackColor = false;
+            this.buttonICMSDIFAL.Click += new System.EventHandler(this.buttonICMSDIFAL_Click);
+            // 
+            // buttonIPI
+            // 
+            this.buttonIPI.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.buttonIPI.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonIPI.FlatAppearance.BorderColor = System.Drawing.Color.DarkSlateGray;
+            this.buttonIPI.FlatAppearance.BorderSize = 0;
+            this.buttonIPI.FlatAppearance.MouseDownBackColor = System.Drawing.Color.CadetBlue;
+            this.buttonIPI.FlatAppearance.MouseOverBackColor = System.Drawing.Color.CadetBlue;
+            this.buttonIPI.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonIPI.Font = new System.Drawing.Font("Nirmala UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonIPI.ForeColor = System.Drawing.Color.White;
+            this.buttonIPI.Location = new System.Drawing.Point(-1, 141);
+            this.buttonIPI.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonIPI.Name = "buttonIPI";
+            this.buttonIPI.Size = new System.Drawing.Size(173, 34);
+            this.buttonIPI.TabIndex = 8;
+            this.buttonIPI.Text = "  IPI";
+            this.buttonIPI.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.buttonIPI.UseVisualStyleBackColor = false;
+            this.buttonIPI.Click += new System.EventHandler(this.buttonIPI_Click);
+            // 
+            // buttonPIS
+            // 
+            this.buttonPIS.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.buttonPIS.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonPIS.FlatAppearance.BorderColor = System.Drawing.Color.DarkSlateGray;
+            this.buttonPIS.FlatAppearance.BorderSize = 0;
+            this.buttonPIS.FlatAppearance.MouseDownBackColor = System.Drawing.Color.CadetBlue;
+            this.buttonPIS.FlatAppearance.MouseOverBackColor = System.Drawing.Color.CadetBlue;
+            this.buttonPIS.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonPIS.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonPIS.ForeColor = System.Drawing.Color.White;
+            this.buttonPIS.Location = new System.Drawing.Point(-1, 179);
+            this.buttonPIS.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonPIS.Name = "buttonPIS";
+            this.buttonPIS.Size = new System.Drawing.Size(173, 34);
+            this.buttonPIS.TabIndex = 9;
+            this.buttonPIS.Text = "  PIS";
+            this.buttonPIS.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.buttonPIS.UseVisualStyleBackColor = false;
+            this.buttonPIS.Click += new System.EventHandler(this.buttonPIS_Click);
+            // 
+            // buttonCOFINS
+            // 
+            this.buttonCOFINS.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.buttonCOFINS.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonCOFINS.FlatAppearance.BorderColor = System.Drawing.Color.DarkSlateGray;
+            this.buttonCOFINS.FlatAppearance.BorderSize = 0;
+            this.buttonCOFINS.FlatAppearance.MouseDownBackColor = System.Drawing.Color.CadetBlue;
+            this.buttonCOFINS.FlatAppearance.MouseOverBackColor = System.Drawing.Color.CadetBlue;
+            this.buttonCOFINS.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonCOFINS.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonCOFINS.ForeColor = System.Drawing.Color.White;
+            this.buttonCOFINS.Location = new System.Drawing.Point(-1, 216);
+            this.buttonCOFINS.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonCOFINS.Name = "buttonCOFINS";
+            this.buttonCOFINS.Size = new System.Drawing.Size(173, 34);
+            this.buttonCOFINS.TabIndex = 10;
+            this.buttonCOFINS.Text = "  COFINS";
+            this.buttonCOFINS.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.buttonCOFINS.UseVisualStyleBackColor = false;
+            this.buttonCOFINS.Click += new System.EventHandler(this.buttonCOFINS_Click);
+            // 
+            // pictureBoxCALC
+            // 
+            this.pictureBoxCALC.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxCALC.Image")));
+            this.pictureBoxCALC.Location = new System.Drawing.Point(248, 36);
+            this.pictureBoxCALC.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBoxCALC.Name = "pictureBoxCALC";
+            this.pictureBoxCALC.Size = new System.Drawing.Size(350, 302);
+            this.pictureBoxCALC.TabIndex = 11;
+            this.pictureBoxCALC.TabStop = false;
+            // 
+            // buttonSOBRE
+            // 
+            this.buttonSOBRE.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.buttonSOBRE.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonSOBRE.FlatAppearance.BorderColor = System.Drawing.Color.DarkSlateGray;
+            this.buttonSOBRE.FlatAppearance.BorderSize = 0;
+            this.buttonSOBRE.FlatAppearance.MouseDownBackColor = System.Drawing.Color.CadetBlue;
+            this.buttonSOBRE.FlatAppearance.MouseOverBackColor = System.Drawing.Color.CadetBlue;
+            this.buttonSOBRE.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonSOBRE.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSOBRE.ForeColor = System.Drawing.Color.White;
+            this.buttonSOBRE.Location = new System.Drawing.Point(-1, 317);
+            this.buttonSOBRE.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonSOBRE.Name = "buttonSOBRE";
+            this.buttonSOBRE.Size = new System.Drawing.Size(173, 27);
+            this.buttonSOBRE.TabIndex = 12;
+            this.buttonSOBRE.Text = "    Sobre a Calculadora";
+            this.buttonSOBRE.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.buttonSOBRE.UseVisualStyleBackColor = false;
+            this.buttonSOBRE.Click += new System.EventHandler(this.buttonSOBRE_Click);
+            // 
+            // buttonINICIO
+            // 
+            this.buttonINICIO.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.buttonINICIO.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonINICIO.FlatAppearance.BorderColor = System.Drawing.Color.DarkSlateGray;
+            this.buttonINICIO.FlatAppearance.BorderSize = 0;
+            this.buttonINICIO.FlatAppearance.MouseDownBackColor = System.Drawing.Color.CadetBlue;
+            this.buttonINICIO.FlatAppearance.MouseOverBackColor = System.Drawing.Color.CadetBlue;
+            this.buttonINICIO.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonINICIO.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonINICIO.ForeColor = System.Drawing.Color.White;
+            this.buttonINICIO.Location = new System.Drawing.Point(-1, 254);
+            this.buttonINICIO.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonINICIO.Name = "buttonINICIO";
+            this.buttonINICIO.Size = new System.Drawing.Size(173, 34);
+            this.buttonINICIO.TabIndex = 13;
+            this.buttonINICIO.Text = "  TELA INICIAL";
+            this.buttonINICIO.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.buttonINICIO.UseVisualStyleBackColor = false;
+            this.buttonINICIO.Click += new System.EventHandler(this.buttonINICIO_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(248, 36);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(358, 299);
+            this.pictureBox1.TabIndex = 14;
+            this.pictureBox1.TabStop = false;
+            // 
+            // TelaPRINCIPAL
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(613, 359);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.buttonINICIO);
+            this.Controls.Add(this.buttonSOBRE);
+            this.Controls.Add(this.pictureBoxCALC);
+            this.Controls.Add(this.buttonCOFINS);
+            this.Controls.Add(this.buttonPIS);
+            this.Controls.Add(this.buttonIPI);
+            this.Controls.Add(this.buttonICMSDIFAL);
+            this.Controls.Add(this.buttonICMSST);
+            this.Controls.Add(this.buttonICMS);
+            this.Controls.Add(this.pictureBoxMENU);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MaximizeBox = false;
+            this.Name = "TelaPRINCIPAL";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Calculadora de Impostos";
+            this.Load += new System.EventHandler(this.TelaINICIAL);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxMENU)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxCALC)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxMENU;
+        private System.Windows.Forms.Button buttonICMS;
+        private System.Windows.Forms.Button buttonICMSST;
+        private System.Windows.Forms.Button buttonICMSDIFAL;
+        private System.Windows.Forms.Button buttonIPI;
+        private System.Windows.Forms.Button buttonPIS;
+        private System.Windows.Forms.Button buttonCOFINS;
+        private System.Windows.Forms.PictureBox pictureBoxCALC;
+        private System.Windows.Forms.Button buttonSOBRE;
+        private System.Windows.Forms.Button buttonINICIO;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Principal_Calculadora/Principal_Calculadora/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Principal_Calculadora
+{
+    public partial class TelaPRINCIPAL : Form
+    {
+        public TelaPRINCIPAL()
+        {
+            InitializeComponent();
+        }
+
+        private void TelaINICIAL(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonICMS_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonICMSST_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonICMSDIFAL_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonIPI_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonPIS_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonCOFINS_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonINICIO_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonSOBRE_Click(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
